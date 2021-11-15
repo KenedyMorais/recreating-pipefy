49a8a867593551729a5b7a5af8b007f4757baeee
Kenedy Morais <kennedy.moraes3@gmail.com>
2021-11-15 02:41:46 -0300

    Initial repo

---
+++ src/Components/Card/index.js
@@ -0,0 +1,54 @@
+import React, { useRef, useContext } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+import { Container, Label } from './styles'
+import BoardContext from '../Board/Context'
+
+export default function Card({ data, index, listIndex }) {
+    const ref = useRef()
+    const { move } = useContext(BoardContext)
+
+    const [{ isDragging }, dragRef] = useDrag(() => ({
+        type: 'CARD',
+        item: { index, listIndex },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging()
+        })
+    }), [])
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor) {
+            const draggedListIndex = item.listIndex
+            const targetListIndex = listIndex
+            const draggedIndex = item.index
+            const targetIndex = index
+
+            if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) return
+
+            const targetSize = ref.current.getBoundingClientRect()
+            const targetCenter = (targetSize.bottom - targetSize.top) / 2
+            const draggedOffset = monitor.getClientOffset()
+            const draggetTop = draggedOffset.y - targetSize.top
+
+            if(draggedIndex < targetIndex && draggetTop < targetCenter) return
+            if(draggedIndex > targetIndex && draggetTop > targetCenter) return
+
+            move(draggedListIndex, targetListIndex, draggedIndex, targetIndex)
+
+            item.index = targetIndex
+            item.listIndex = targetListIndex
+        }
+    })
+
+    dragRef(dropRef(ref))
+
+    return (
+        <Container ref={ref} isDragging={isDragging}>
+            <header>
+                {data.labels.map(label => <Label key={label} color={label} />)}         
+            </header>
+            <p>{data.content}</p>
+            { data.user && <img src={data.user} />  }
+        </Container>
+    )
+}
